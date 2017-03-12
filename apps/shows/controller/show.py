542f9a1939ad8cd953185a0e4f89893a330f42a6
lightless <lightless@foxmail.com>
2017-03-12 14:58:24 +0800

    侧边栏RSS源分组显示

--- apps/shows/controller/show.py
+++ apps/shows/controller/show.py
@@ -16,7 +16,7 @@
 from django.views import View
 from django.shortcuts import render
 
-from apps.db.models import BzArticles, BzSource
+from apps.db.models import BzArticles, BzSource, BzSourceGroup
 
 
 class ShowIndex(View):
@@ -27,9 +27,27 @@ class ShowIndex(View):
         articles = BzArticles.objects.filter(is_deleted=0).all()[:10]
         rss_source = BzSource.objects.filter(is_deleted=0).all()
 
+        rss_result = dict()
+        for each_rss in rss_source:
+
+            # 获取group name
+            gid = each_rss.group_id
+            source_group_qs = BzSourceGroup.objects.filter(is_deleted=0, id=gid).first()
+            if not source_group_qs:
+                group_name = "NoGroup"
+            else:
+                group_name = source_group_qs.group_name
+
+            # 添加到rss_result中
+            if group_name in rss_result.keys():
+                rss_result[group_name].append(each_rss)
+            else:
+                rss_result[group_name] = list()
+                rss_result[group_name].append(each_rss)
+
         context = {
             "articles": articles,
-            "rss_sources": rss_source,
+            "rss_sources": rss_result,
         }
         return render(request, "shows/show.html", context=context)
 
--- baize/wsgi.py
+++ baize/wsgi.py
@@ -19,5 +19,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "baize.settings")
 application = get_wsgi_application()
 
 # user's entry code
-share_memory.spider_engine = SpiderEngine()
+# share_memory.spider_engine = SpiderEngine()
 
